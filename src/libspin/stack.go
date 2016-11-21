1980644032998453ca81f47ffaaafe32a4aa7c2a
Ikey Doherty <ikey@solus-project.com>
2016-11-21 18:42:07 +0000

    libspin: Start organising stacks
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/libspin/stack.go
+++ src/libspin/stack.go
@@ -16,19 +16,35 @@
 
 package libspin
 
+// OpStack contains all top level blocks
+type OpStack struct {
+	Blocks []*OpSet
+}
+
+// OpSet has a given type of operations that it supports
+type OpSet struct {
+	Ops []*Operation
+}
+
+// Operation is the base Op type
+type Operation interface{}
+
 // An OpRepo is an operation to enable a repository on a target
 type OpRepo struct {
+	Operation
 	RepoName string
 	RepoURI  string
 }
 
 // An OpGroup is an operation to install a group/component
 type OpGroup struct {
+	Operation
 	GroupName string
 }
 
 // An OpPackage is an operation to install a given package
 type OpPackage struct {
+	Operation
 	Name         string
 	IgnoreSafety bool
 }
