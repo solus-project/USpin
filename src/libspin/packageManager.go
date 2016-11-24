cded8a9a5583be1d7d83dd7fcb6101bb88ef5fde
Ikey Doherty <ikey@solus-project.com>
2016-11-24 01:48:45 +0000

    libspin: Add the abstract concept of PackageManager
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/libspin/packageManager.go
@@ -0,0 +1,53 @@
+//
+// Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package libspin
+
+import (
+	"errors"
+	"libspin/config"
+	"libspin/spec"
+)
+
+// PackageManager is the interface that should be implemented by vendors to enable
+// solspin to understand them and construct images according to their particulars.
+type PackageManager interface {
+	// Init will allwo implementations to initialise themselves according to any
+	// particulars in the image configuration, allowing it to make better informed
+	// decisions later on.
+
+	Init(conf *config.ImageConfiguration) error
+	// InitRoot implementations should set up the root filesystem to handle any
+	// quirks prior to installing packages. This also allows manipulating the
+	// filesystem layout, i.e. for usr-merge situations, or for working around
+	// default directories created by a host-side package manager tool.
+	InitRoot() error
+
+	// ApplyOperations should apply all of the given operations in bulk, as they
+	// are always gauranteed to have the same type.
+	ApplyOperations(ops []spec.Operation) error
+
+	// Cleanup may be called at any time, and the package manager implementation
+	// should ensure it cleans anything it did in the past, such as closing open
+	// processes.
+	Cleanup() error
+}
+
+// NewPackageManager will return an appropriate package manager instance for
+// the given name, if it exists.
+func NewPackageManager(name string) (PackageManager, error) {
+	return nil, errors.New("Not yet implemented")
+}
