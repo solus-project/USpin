d0dd890104a33f29e0426971d869179bf9d6b7c6
Ikey Doherty <ikey@solus-project.com>
2016-11-24 02:30:44 +0000

    libimage: Add utility to create a sparse image easily
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/libimage/main.go
+++ src/libimage/main.go
@@ -16,3 +16,27 @@
 
 // Package libimage provides routines for the core elements of image building
 package libimage
+
+import (
+	"os"
+	"syscall"
+)
+
+// CreateSparseFile will create a new sparse file with the given filename and
+// size in nMegabytes/
+//
+// This is highly dependent on the underlying filesystem at the directory
+// where the file is to be created, making use of the syscall ftruncate.
+func CreateSparseFile(filename string, nMegabytes int) error {
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 00644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	// NOTE: New megabytes, not old megabytes (1000, not 1024)
+	sz := int64(nMegabytes * 1000 * 1000)
+	if err = syscall.Ftruncate(int(f.Fd()), sz); err != nil {
+		return err
+	}
+	return nil
+}
