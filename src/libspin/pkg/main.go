b736f534adaa7b976e5b695170d979e2086f1f14
Ikey Doherty <ikey@solus-project.com>
2016-11-25 04:40:26 +0000

    pkg: Move docs into doc.go
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/libspin/pkg/main.go
+++ src/libspin/pkg/main.go
@@ -14,28 +14,6 @@
 // limitations under the License.
 //
 
-// Package pkg provides package management implementations.
-//
-// Package management
-//
-// This library abstracts the details of different package managers in order
-// to allow the tool to remain agnostic. Each package manager implementation
-// follows a specific contract through the Manager interface.
-//
-// Usage
-//
-// Using a package manager instance is as simple as gaining a reference to it,
-// via the NewPackageManager method.
-//      manager, err := pkg.NewManager(pkg.PackageManagerEopkg)
-//
-// Placement in the lifecycle
-//
-// The package manager instance is only called upon after the initial root filesystem
-// has been initialised by the Builder. During this time, every method of the API
-// will be called.
-// Implementations should not expect longevity, as soon as the operation set has
-// been completed, the rootfs is then finalized ready for packing in whichever form
-// is most appropriate to the builder.
 package pkg
 
 import (
