17d3fca306627a8106b14f6cb3f3e2810de39900
Ikey Doherty <ikey@solus-project.com>
2016-11-29 02:36:34 +0000

    dracut: List the required modules & drivers for dracut
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/libuspin/boot/dracut.go
+++ src/libuspin/boot/dracut.go
@@ -20,13 +20,45 @@ import (
 	"fmt"
 )
 
+var (
+	// DracutLiveOSModules are modules to enable for Live OS Usage
+	// TODO: Make systemd dependent on the presence of systemd in the sysroot
+	DracutLiveOSModules = []string{"dmsquash-live", "systemd", "pollcdrom"}
+
+	// DracutLiveOSDrivers are drivers that should be shipped for LiveOS functionality to work
+	// TODO: Investigate now-dead stuff and curate this list
+	DracutLiveOSDrivers = []string{
+		"squashfs",
+		"ext3",
+		"ext2",
+		"vfat",
+		"msdos",
+		"sr_mod",
+		"sd_mod",
+		"ehci_hcd",
+		"uhci_hcd",
+		"xhci_hcd",
+		"xhci_pci",
+		"ohci_hcd",
+		"usb_storage",
+		"usbhid",
+		"dm_mod",
+		"device-mapper",
+		"ata_generic",
+		"libata",
+	}
+)
+
 // Dracut provides wrapping around dracut generation in chroots
 type Dracut struct {
 	// Additional options to pass to dracut when generating
 	Options []string
 
-	// Extract modules to enable
+	// Extra modules to enable
 	Modules []string
+
+	// Extra drivers to enable
+	Drivers []string
 
 	// The filename to use within the root
 	OutputFilename string
